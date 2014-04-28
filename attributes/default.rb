a510bae750d4edc3b6219898871f50d4c868a269
Sliim <sliim@mailoo.org>
2014-04-28 23:26:24 +0200

    Changed license for Apache 2.0

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,19 @@
+#
+# Cookbook Name:: emacs24
+# Attributes:: default
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 default["emacs24"]["build_dir"] = "/opt/emacs24"
 default["emacs24"]["version"] = "24.3"
