cfc9495605268eaf7a609af9cacbae403c192d64
Sliim <sliim@mailoo.org>
2014-10-22 09:56:42 +0200

    Change default emacs version to 24.4

--- attributes/default.rb
+++ attributes/default.rb
@@ -17,7 +17,7 @@
 #
 
 default['emacs24']['build_dir'] = '/opt/emacs24'
-default['emacs24']['version'] = '24.3'
+default['emacs24']['version'] = '24.4'
 default['emacs24']['packages'] = []
 
 case node['platform']
