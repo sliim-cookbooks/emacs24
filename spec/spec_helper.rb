3ffc54448a3b070bc1130e813eca8fdf287ed15a
Sliim <sliim@mailoo.org>
2020-01-08 23:11:40 +0100

    Specify debian 9.11 fauxhai platform for ChefSpec Runner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,8 @@ require 'chefspec/berkshelf'
 
 RSpec.configure do |config|
   config.log_level = :error
+  config.platform = 'debian'
+  config.version = '9.11'
 end
 
 ChefSpec::Coverage.start! { add_filter 'emacs24' }
